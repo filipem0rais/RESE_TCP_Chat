6877ea68d3a4ae145c22ebf07748b1a360a92207
Dias Morais Filipe <diasmorais.filipe@gmail.com>
2022-03-08 11:40:33 +0100

    Simple Connexion

---
+++ TCP.py
@@ -0,0 +1,47 @@
+'''
+    Fichier: TCP.py
+    Auteur: Filipe Dias Morais
+    Date création: 08.03.2022
+    Python Version: 3.9
+    
+    Objectif : 
+    Commentaire : 
+
+
+Algorithme :
+--------------
+
+
+'''
+
+'''
+Programme principal
+-------------------
+'''
+
+import socket
+
+HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
+PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
+
+isServer = input("Serveur (1) ou client(0) ?")
+
+if isServer == "1":
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((HOST, PORT))
+        s.listen()
+        conn, addr = s.accept()
+        with conn:
+            print(f"Connected by {addr}")
+            while True:
+                data = conn.recv(1024)
+                if not data:
+                    break
+                conn.sendall(data)
+else:
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((HOST, PORT))
+        s.sendall(b"Hello, world")
+        data = s.recv(1024)
+
+    print(f"Received {data!r}")
